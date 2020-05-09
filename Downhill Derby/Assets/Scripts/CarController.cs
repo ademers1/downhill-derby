3b0b8dd18b89c2c70f90274a13445483d9295966
ademers1 <ademers145@live.ca>
2020-05-09 17:55:26 -0400

    Added movement to car
    added in the car movement using wheels

---
+++ Downhill Derby/Assets/Scripts/CarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class CarController : MonoBehaviour
+{
+    public InputManager im;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 20000f;
+    public float maxTurn = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        im = GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach(WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.fixedDeltaTime * im.throttle;
+        }
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * im.steer;
+        }
+    }
+}
