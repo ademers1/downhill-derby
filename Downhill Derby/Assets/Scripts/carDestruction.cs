0ddf2513b51c8a4893d2b0033938b215ab1756f5
TooFairytale <53872823+TooFairytale@users.noreply.github.com>
2020-05-09 18:01:19 -0400

    Car destruction script basic done

---
+++ Downhill Derby/Assets/Scripts/carDestruction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carDestruction : MonoBehaviour
+{
+    public GameObject parent;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag =="Obstacles")
+        {
+            parent.transform.GetChild(1).gameObject.transform.parent = null;
+        }
+    }
+}
