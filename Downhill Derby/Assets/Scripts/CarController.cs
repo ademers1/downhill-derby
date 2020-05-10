0f173633e215b10ab5eb60f8ac818306d11ce76d
ademers1 <ademers145@live.ca>
2020-05-10 18:44:18 -0400

    Updated the old project with this new one
    Created a new project and am updating old one with it

--- Downhill Derby/Assets/Scripts/CarController.cs
+++ Downhill Derby/Assets/Scripts/CarController.cs
@@ -3,30 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(Rigidbody))]
 public class CarController : MonoBehaviour
 {
     public InputManager im;
     public List<WheelCollider> throttleWheels;
-    public List<WheelCollider> steeringWheels;
+    public List<GameObject> steeringWheels;
+    public List<GameObject> meshes;
     public float strengthCoefficient = 20000f;
     public float maxTurn = 0.1f;
+    public Transform CM;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         im = GetComponent<InputManager>();
+        rb = GetComponent<Rigidbody>();
+
+        if (CM)
+        {
+           // rb.centerOfMass = CM.localPosition;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(rb.velocity.magnitude > 10)
+        {
+            AkSoundEngine.SetRTPCValue("driveSpeed", 50f);
+        }
         foreach(WheelCollider wheel in throttleWheels)
         {
             wheel.motorTorque = strengthCoefficient * Time.fixedDeltaTime * im.throttle;
         }
-        foreach(WheelCollider wheel in steeringWheels)
+        foreach(GameObject wheel in steeringWheels)
         {
-            wheel.steerAngle = maxTurn * im.steer;
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn, 0f);
+        }
+        foreach (GameObject mesh in meshes)
+        {
+            mesh.transform.Rotate(mesh.transform.right * rb.velocity.magnitude / (2 * Mathf.PI * 0.08f) , 0f, 0f);
         }
     }
 }
---
+++ Downhill Derby/Assets/Scripts/CreateLevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLevel : MonoBehaviour
+{
+    public GameObject LevelPrefab;
+    public GameObject OldLevel = null;
+    public GameObject Player;
+    Queue<GameObject> pool = new Queue<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (LevelPrefab)
+        {
+            pool.Enqueue(LevelPrefab);
+        }
+        for (int i = 0; i <5; i++)
+        {
+            LevelPrefab = Instantiate(LevelPrefab, (LevelPrefab.transform.position + new Vector3(0, -16.38f, 45.01f)), LevelPrefab.transform.rotation);
+            pool.Enqueue(LevelPrefab);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OldLevel = pool.Dequeue();
+        if (Player.transform.position.y < OldLevel.transform.position.y - 100 && Player.transform.position.z > OldLevel.transform.position.z + 100)
+        {
+            OldLevel.transform.position = LevelPrefab.transform.position + new Vector3(0, -16.38f, 45.01f);
+            LevelPrefab = OldLevel;
+        }
+        pool.Enqueue(OldLevel);
+    }
+}
