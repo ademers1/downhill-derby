27894e97d6d7a8300721d98dd486dd5a8849bf01
ademers1 <ademers145@live.ca>
2020-05-10 20:16:11 -0400

    Added score
    Added a score to my scene

--- Downhill Derby/Assets/Scripts/Score.cs
+++ Downhill Derby/Assets/Scripts/Score.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    float scoreMultiplier = 1f;
+    float score;
+    public GameObject Player;
+    public Text txtScore;
+    int oldScore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        oldScore = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        score += Player.GetComponent<Rigidbody>().velocity.magnitude * scoreMultiplier;
+        if(score / 100 > oldScore)
+        {
+            oldScore = (int) score/100;
+
+            txtScore.text = "Score: " + oldScore.ToString() + "00";
+            
+        }
     }
 }
